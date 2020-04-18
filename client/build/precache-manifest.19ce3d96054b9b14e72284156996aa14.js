85e85ab4552872353ac8b27a436771a02712c3f8
Ian Hooper <ianhooper14@gmail.com>
2020-04-18 14:16:01 -0400

    updated for deployment

---
+++ client/build/precache-manifest.19ce3d96054b9b14e72284156996aa14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5819cc5a2411284fac05",
+    "url": "/static/css/main.54a700ac.chunk.css"
+  },
+  {
+    "revision": "5819cc5a2411284fac05",
+    "url": "/static/js/main.cbac3f1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "6f928c15bcf20e5496a1bb15a7e81b54",
+    "url": "/index.html"
+  }
+];
